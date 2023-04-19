efc7bb5bc78c468acba9f507c82b46a225dcf057
Taimoork8 <kingtaimoor405@gmail.com>
2023-04-19 18:09:10 +0500

    step-3

--- lib/example3.dart
+++ lib/example3.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 class ExampleThree extends HookWidget {
@@ -6,11 +7,33 @@ class ExampleThree extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Ecample 3'),
-      ),
+    final future = useMemoized(
+      () => NetworkAssetBundle(Uri.parse(url))
+          .load(url)
+          .then((data) => data.buffer.asUint8List())
+          .then((data) => Image.memory(data)),
     );
+    final snapshot = useFuture(future);
+
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Ecample 3'),
+        ),
+        body: Column(
+          children: [snapshot.data].compactMap().toList(),
+        ));
   }
 }
+
+extension CompactMap<T> on Iterable<T?> {
+  Iterable<T> compactMap<E>([
+    E? Function(T?)? transform,
+  ]) =>
+      map(
+        transform ?? (e) => e,
+      ).where((e) => e != null).cast();
+}
+
+const url =
+    'https://images.pexels.com/photos/39853/woman-girl-freedom-happy-39853.jpeg';
